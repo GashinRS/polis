f46b9123514742ca1709b73f7168adc90337d7ef
Sheng Tao <shengtao.tian@ugent.be>
2021-04-08 11:11:31 +0200

    main methode toegevoegd aan main klasse

--- src/polis/Main.java
+++ src/polis/Main.java
@@ -4,12 +4,16 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Polis extends Application {
+public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
         stage.setScene(new Scene (new CityContainer()));
         stage.setTitle("Polis - 2021 © Universiteit Gent");
         stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
     }
 }
